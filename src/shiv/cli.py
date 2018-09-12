1a2740a4757de1b4efa79db1a3c6c61315693a7e
Loren Carvalho <lcarvalho@linkedin.com>
2018-09-11 20:36:44 -0700

    Bump version: 0.0.31 → 0.0.32

--- src/shiv/cli.py
+++ src/shiv/cli.py
@@ -26,7 +26,7 @@ from .constants import (
     NO_ENTRY_POINT,
 )
 
-__version__ = '0.0.31'
+__version__ = '0.0.32'
 
 # This is the 'knife' emoji
 SHIV = u"\U0001F52A"
@@ -126,8 +126,7 @@ def main(
 
         if site_packages:
             shutil.copytree(site_packages, tmp_site_packages)
-        else:
-            tmp_site_packages.mkdir(parents=True, exist_ok=True)
+
         if pip_args:
             # install deps into staged site-packages
             pip.install(["--target", str(tmp_site_packages)] + list(pip_args))
