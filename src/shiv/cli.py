3b1d9a38330b617b2da2faf34f887d9bf7540a83
Loren Carvalho <lcarvalho@linkedin.com>
2018-10-24 23:45:04 -0500

    Don't build shiv's with virtualenv's python.
    Fixes #69

--- src/shiv/cli.py
+++ src/shiv/cli.py
@@ -26,7 +26,7 @@ from .constants import (
     NO_ENTRY_POINT,
 )
 
-__version__ = '0.0.36'
+__version__ = "0.0.36"
 
 # This is the 'knife' emoji
 SHIV = u"\U0001F52A"
@@ -63,16 +63,37 @@ def copy_bootstrap(bootstrap_target: Path) -> None:
                 shutil.copyfile(f.absolute(), bootstrap_target / f.name)
 
 
-def _interpreter_path():
+def _interpreter_path(append_version: bool = False) -> str:
     """A function to return the path to the current Python interpreter.
 
-    Even when inside a virtualenv, this will return the interpreter the venv was created with.
+    Even when inside a venv, this will return the interpreter the venv was created with.
 
     """
 
-    return str(
-        Path(getattr(sys, 'real_prefix', sys.base_prefix)) / Path(sys.executable).relative_to(sys.prefix)
-    )
+    base_dir = Path(getattr(sys, "real_prefix", sys.base_prefix)).resolve()
+    sys_exec = Path(sys.executable)
+    name = sys_exec.stem
+    suffix = sys_exec.suffix
+
+    if append_version:
+        name += str(sys.version_info.major)
+
+    name += suffix
+
+    try:
+        return str(next(iter(base_dir.rglob(name))))
+
+    except StopIteration:
+
+        if not append_version:
+            # If we couldn't find an interpreter, it's likely that we looked for
+            # "python" when we should've been looking for "python3"
+            # so we try again with append_version=True
+            return _interpreter_path(append_version=True)
+
+        # If we were still unable to find a real interpreter for some reason
+        # we fallback to the current runtime's interpreter
+        return sys.executable
 
 
 @click.command(
@@ -80,7 +101,7 @@ def _interpreter_path():
         help_option_names=["-h", "--help", "--halp"], ignore_unknown_options=True
     )
 )
-@click.version_option(version=__version__, prog_name='shiv')
+@click.version_option(version=__version__, prog_name="shiv")
 @click.option("--entry-point", "-e", default=None, help="The entry point to invoke.")
 @click.option(
     "--console-script", "-c", default=None, help="The console_script to invoke."
@@ -118,7 +139,11 @@ def main(
     as outlined in PEP 441, but with all their dependencies included!
     """
 
+<<<<<<< HEAD
     quiet = "-q" in pip_args or '--quiet' in pip_args
+=======
+    quiet = "-q" in pip_args or "--quiet" in pip_args
+>>>>>>> Don't build shiv's with virtualenv's python.
 
     if not quiet:
         click.secho(" shiv! " + SHIV, bold=True)
