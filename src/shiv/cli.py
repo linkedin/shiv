7cb64d5411c7dee1374db50f71e53e80e60694b9
Jarek Rudzinski <jarekr@wepay.com>
2019-11-01 13:18:06 -0700

    make python3.6 happy

--- src/shiv/cli.py
+++ src/shiv/cli.py
@@ -127,7 +127,7 @@ def main(
                 # the dst dir arg to shutil.copytree must not exist, so tack on a non-existant directory here. Starting
                 # with python 3.8 this can be removed and shutil.copytree called with dirs_exist_ok=True to work around this
                 tmp_site_packages = Path(tmp_site_packages, "site_packages")
-                shutil.copytree(site_packages, tmp_site_packages)
+                shutil.copytree(site_packages, str(tmp_site_packages))
             else:
                 tmp_site_packages = site_packages
 
