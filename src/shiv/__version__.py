linkedin/shiv

commit 377c70db139a7e1fc5070a2a294d1fcd731dfc55

Feature: SHIV_PREPEND_PYTHONPATH (#254)
* Add SHIV_PREPEND_PYTHONPATH and environment type-safety

* Bump version 1.0.7

src/shiv/__version__.py
@@ -1,1 +1,1 @@
-__version__ = "1.0.6"
+__version__ = "1.0.7"

src/shiv/bootstrap/environment.py
@@ -4,98 +4,107 @@ overrides defined at runtime (via environment variables).
 """
 import json
 import os
+from typing import Any, Optional
 
 
-def str_bool(v):
+def str_bool(v) -> bool:
     if not isinstance(v, bool):
         return str(v).lower() in ("yes", "true", "t", "1")
     return v
 
 
 class Environment:
-    INTERPRETER = "SHIV_INTERPRETER"
-    ENTRY_POINT = "SHIV_ENTRY_POINT"
-    CONSOLE_SCRIPT = "SHIV_CONSOLE_SCRIPT"
-    MODULE = "SHIV_MODULE"
-    ROOT = "SHIV_ROOT"
-    FORCE_EXTRACT = "SHIV_FORCE_EXTRACT"
-    COMPILE_PYC = "SHIV_COMPILE_PYC"
-    COMPILE_WORKERS = "SHIV_COMPILE_WORKERS"
-    EXTEND_PYTHONPATH = "SHIV_EXTEND_PYTHONPATH"
+    INTERPRETER: str = "SHIV_INTERPRETER"
+    ENTRY_POINT: str = "SHIV_ENTRY_POINT"
+    CONSOLE_SCRIPT: str = "SHIV_CONSOLE_SCRIPT"
+    MODULE: str = "SHIV_MODULE"
+    ROOT: str = "SHIV_ROOT"
+    FORCE_EXTRACT: str = "SHIV_FORCE_EXTRACT"
+    COMPILE_PYC: str = "SHIV_COMPILE_PYC"
+    COMPILE_WORKERS: str = "SHIV_COMPILE_WORKERS"
+    EXTEND_PYTHONPATH: str = "SHIV_EXTEND_PYTHONPATH"
+    PREPEND_PYTHONPATH: str = "SHIV_PREPEND_PYTHONPATH"
 
     def __init__(
         self,
-        built_at,
-        shiv_version,
-        always_write_cache=False,
-        build_id=None,
-        compile_pyc=True,
-        entry_point=None,
-        extend_pythonpath=False,
-        hashes=None,
-        no_modify=False,
-        reproducible=False,
-        script=None,
-        preamble=None,
-        root=None,
-    ):
-        self.always_write_cache = always_write_cache
-        self.build_id = build_id
-        self.built_at = built_at
-        self.hashes = hashes or {}
-        self.no_modify = no_modify
-        self.reproducible = reproducible
-        self.shiv_version = shiv_version
-        self.preamble = preamble
+        built_at: str,
+        shiv_version: str,
+        always_write_cache: bool = False,
+        build_id: Optional[str] = None,
+        compile_pyc: bool = True,
+        entry_point: Optional[str] = None,
+        extend_pythonpath: bool = False,
+        prepend_pythonpath: Optional[str] = None,
+        hashes: Optional[dict[str, Any]] = None,
+        no_modify: bool = False,
+        reproducible: bool = False,
+        script: Optional[str] = None,
+        preamble: Optional[str] = None,
+        root: Optional[str] = None,
+    ) -> None:
+        self.shiv_version: str = shiv_version
+        self.always_write_cache: bool = always_write_cache
+        self.build_id: Optional[str] = build_id
+        self.built_at: str = built_at
+        self.hashes: Optional[dict[str, Any]] = hashes or {}
+        self.no_modify: bool = no_modify
+        self.reproducible: bool = reproducible
+        self.preamble: Optional[str] = preamble
 
         # properties
-        self._entry_point = entry_point
-        self._compile_pyc = compile_pyc
-        self._extend_pythonpath = extend_pythonpath
-        self._root = root
-        self._script = script
+        self._entry_point: Optional[str] = entry_point
+        self._compile_pyc: bool = compile_pyc
+        self._extend_pythonpath: bool = extend_pythonpath
+        self._prepend_pythonpath: Optional[str] = prepend_pythonpath
+        self._root: Optional[str] = root
+        self._script: Optional[str] = script
 
     @classmethod
-    def from_json(cls, json_data):
+    def from_json(cls, json_data) -> "Environment":
         return Environment(**json.loads(json_data))
 
-    def to_json(self):
+    def to_json(self) -> str:
         return json.dumps(
             # we strip the leading underscores to retain properties (such as _entry_point)
             {key.lstrip("_"): value for key, value in self.__dict__.items()}
         )
 
     @property
-    def entry_point(self):
+    def entry_point(self) -> Optional[str]:
         return os.environ.get(self.ENTRY_POINT, os.environ.get(self.MODULE, self._entry_point))
 
     @property
-    def script(self):
+    def script(self) -> Optional[str]:
         return os.environ.get(self.CONSOLE_SCRIPT, self._script)
 
     @property
-    def interpreter(self):
+    def interpreter(self) -> Optional[str]:
         return os.environ.get(self.INTERPRETER, None)
 
     @property
-    def root(self):
+    def root(self) -> Optional[str]:
         root = os.environ.get(self.ROOT, self._root)
         return root
 
     @property
-    def force_extract(self):
+    def force_extract(self) -> bool:
         return str_bool(os.environ.get(self.FORCE_EXTRACT, self.always_write_cache))
 
     @property
-    def compile_pyc(self):
+    def compile_pyc(self) -> bool:
         return str_bool(os.environ.get(self.COMPILE_PYC, self._compile_pyc))
 
     @property
-    def extend_pythonpath(self):
+    def extend_pythonpath(self) -> Optional[bool]:
         return str_bool(os.environ.get(self.EXTEND_PYTHONPATH, self._extend_pythonpath))
 
     @property
-    def compile_workers(self):
+    def prepend_pythonpath(self) -> Optional[str]:
+        """Prepend the given path to sys.path."""
+        return os.environ.get(self.PREPEND_PYTHONPATH, self._prepend_pythonpath)
+
+    @property
+    def compile_workers(self) -> int:
         try:
             return int(os.environ.get(self.COMPILE_WORKERS, 0))
         except ValueError:

src/shiv/bootstrap/__init__.py
@@ -180,6 +180,12 @@ def ensure_no_modify(site_packages, hashes):
             )
 
 
+def prepend_pythonpath(env):
+    """Prepend the sys.path with the value of SHIV_PREPEND_PYTHONPATH, if set."""
+    if env.prepend_pythonpath:
+        sys.path.insert(0, env.prepend_pythonpath)
+
+
 def bootstrap():  # pragma: no cover
     """Actually bootstrap our shiv environment."""
 
@@ -217,6 +223,9 @@ def bootstrap():  # pragma: no cover
 
     # reorder to place our site-packages before any others found
     sys.path = sys.path[:index] + sys.path[length:] + sys.path[index:length]
+
+    # Prepend the sys.path if environment variable is set
+    prepend_pythonpath(env)
 
     # determine newly added paths
     new_paths = [p for p in sys.path if p not in sys_path_before]

test/test_bootstrap.py
@@ -19,6 +19,7 @@ from shiv.bootstrap import (
     extract_site_packages,
     get_first_sitedir_index,
     import_string,
+    prepend_pythonpath,
 )
 from shiv.bootstrap.environment import Environment
 from shiv.bootstrap.filelock import FileLock
@@ -117,6 +118,25 @@ class TestBootstrap:
         extend_python_path(env, ["test", ".pth"])
         assert env["PYTHONPATH"] == os.pathsep.join(["hello", "test", ".pth"])
 
+    @pytest.mark.parametrize("extra_path", [
+        None,
+        "/path/to/other_package",
+    ])
+    def test_prepend_pythonpath(self, env, extra_path):
+        # Save old path to be able to restore it after executing the test
+        old_path = sys.path.copy()
+
+        env._prepend_pythonpath = extra_path
+        prepend_pythonpath(env)
+        if extra_path is not None:
+            assert len(sys.path) == len(old_path) + 1
+            assert sys.path[0] == extra_path
+        else:
+            assert len(sys.path) == len(old_path)
+
+        # Cleanup
+        sys.path = old_path
+
 
 class TestEnvironment:
     def test_overrides(self, env_var):
@@ -162,6 +182,10 @@ class TestEnvironment:
         # ensure that non-digits are ignored
         with env_var("SHIV_COMPILE_WORKERS", "one bazillion"):
             assert env.compile_workers == 0
+
+        assert env.prepend_pythonpath is None
+        with env_var(Environment.PREPEND_PYTHONPATH, "/path/to/other_package"):
+            assert env.prepend_pythonpath == "/path/to/other_package"
 
     def test_roundtrip(self):
         now = str(datetime.now())

commit 6e444381f3952f831aeec44e7828326225731218

Increase min Python version requirement to 3.7+ and fix py37 compatibility regression in commit 377c70d
This change enables support in Python 3.7 for deferred type evaluation as well as increases the minimum Python version to 3.7.

setup.cfg
@@ -50,8 +50,7 @@ install_requires =
   click>=6.7,!=7.0
   pip>=9.0.3
   setuptools
-  importlib_resources; python_version < "3.7"
-python_requires = >=3.6
+python_requires = >=3.7
 include_package_data = True
 
 [options.extras_require]

src/shiv/__version__.py
@@ -1,1 +1,1 @@
-__version__ = "1.0.7"
+__version__ = "1.1.0"

src/shiv/bootstrap/environment.py
@@ -2,6 +2,7 @@
 This module contains the ``Environment`` object, which combines settings decided at build time with
 overrides defined at runtime (via environment variables).
 """
+from __future__ import annotations
 import json
 import os
 from typing import Any, Optional